3ba3e60471cb953475cf96cb68ab6363950448bf
Rafael Montenegro M. da Silva <88899571+rafamms013@users.noreply.github.com>
2022-06-28 23:38:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Calculadora de Média")
+
+nota1 = float(input("Digite a sua primeira nota: "))
+nota2 = float(input("Digite a sua segunda nota: "))
+
+soma = nota1 + nota2
+
+quantidade = 2
+1
+media = soma / quantidade
+
+print(f'A média do aluno é {media}')
+
